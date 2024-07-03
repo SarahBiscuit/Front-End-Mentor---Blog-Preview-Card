85e3d4f1f4f803b2cecb03a839cb3b6ce9c7e64b
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-07-03 21:45:11 +0100

    Javascript added to make the button work.  Need to now write the media query to resize and reposition things (one for tablet and one for mobiles)
    Have used an eventListener to get the button to work and link to a blog webpage.

---
+++ main.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("learningBtn");
+
+button.addEventListener("click", function(){
+    document.location.href = 'https://www.blogger.com/about/?r=1-null_user';
+});
